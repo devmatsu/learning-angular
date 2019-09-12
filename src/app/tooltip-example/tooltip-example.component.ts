b708805605310a547609cd2eea947721e913a286
Rodrigo Matsuhiro <devmatsu@hotmail.com>
2019-09-12 17:28:38 -0300

    updating

--- src/app/tooltip-example/tooltip-example.component.ts
+++ src/app/tooltip-example/tooltip-example.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+ 
 @Component({
   selector: 'app-tooltip-example',
   templateUrl: './tooltip-example.component.html',
   styleUrls: ['./tooltip-example.component.css']
 })
-export class TooltipExampleComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class TooltipExampleComponent {
+ 
+  public visible = false;
+  public mouse = { x: 0, y: 0 };
+ 
+  alteraPosicaoDaTooltip(x: number, y: number) {
+    this.mouse = { x, y };
   }
-
+ 
+  exibeTooltip() {
+    this.visible = true;
+  }
+ 
+  escondeTooltip() {
+    this.visible = false;
+  }
 }
